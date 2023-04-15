f2cd28a7fb56d60bd80a10777db75d21b252f5de
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-15 12:43:47 +0530

    Update crossing_team_greedy.py

--- multiagent/scenarios/crossing_team_greedy.py
+++ multiagent/scenarios/crossing_team_greedy.py
@@ -247,9 +247,9 @@ class Scenario(BaseScenario):
 			agent.state.p_pos[1] = 1.0
 
 		curr_agent_index = world.agents.index(agent)
-		curr_agent_id = world.agents.index(agent)
+		curr_agent_id = np.array(world.agents.index(agent))
 		# curr_agent_id = np.array(self.agent_ids[curr_agent_index])
-		curr_agent_team_id = agent.team_id
+		curr_agent_team_id = np.array(agent.team_id)
 		# curr_agent_team_id = np.array(self.team_ids[agent.team_id])
 
 		current_agent_critic = [curr_agent_id, curr_agent_team_id, agent.state.p_pos, agent.state.p_vel, world.landmarks[curr_agent_index].state.p_pos]
@@ -260,11 +260,11 @@ class Scenario(BaseScenario):
 				continue
 			# agent_id, relative pose, velocity wrt current_agent and team id of other agent 
 			# agent_id = np.array(self.agent_ids[world.agents.index(other_agent)])
-			agent_id = world.agents.index(other_agent)
+			agent_id = np.array(world.agents.index(other_agent))
 			relative_pose = other_agent.state.p_pos-agent.state.p_pos
 			relative_vel = other_agent.state.p_vel-agent.state.p_vel
 			# agent_team_id = np.array(self.team_ids[other_agent.team_id])
-			agent_team_id = other_agent.team_id
+			agent_team_id = np.array(other_agent.team_id)
 			current_agent_actor.extend([agent_id, agent_team_id, relative_pose, relative_vel])
 
 		return np.concatenate(current_agent_critic, axis=-1), np.concatenate(current_agent_actor, axis=-1)
